a012a13f1b1e82255e7648aa379a1a086ad56d9e
zdx0823 <15916965182@163.com>
2021-04-07 00:24:54 +0800

    添加csrf白名单，测试路由可用

--- app/Http/Controllers/clodedisk/DiskController.php
+++ app/Http/Controllers/clodedisk/DiskController.php
@@ -8,23 +8,37 @@ use Illuminate\Http\Request;
 class DiskController extends Controller
 {
     // 文件，文件夹列表
-    public function show (Request $request) {}
+    public function show (Request $request) {
+        return 'show';
+    }
 
     // 新建文件夹
-    public function storeFolder (Request $request) {}
+    public function storeFolder (Request $request) {
+        return 'storeFolder';
+    }
 
     // 上传文件
-    public function upload (Request $request) {}
+    public function upload (Request $request) {
+        return 'upload';
+    }
 
     // 修改文件名
-    public function updateFileName (Request $request) {}
+    public function updateFileName (Request $request) {
+        return 'updateFileName';
+    }
     
     // 修改文件夹名
-    public function updateFolderName (Request $request) {}
+    public function updateFolderName (Request $request) {
+        return 'updateFolderName';
+    }
     
     // 复制，剪切文件或文件夹
-    public function changeResource (Request $request) {}
+    public function changeResource (Request $request) {
+        return 'changeResource';
+    }
     
     // 删除文件或文件夹
-    public function destroy (Request $request) {}
+    public function destroy (Request $request) {
+        return 'destroy';
+    }
 }
